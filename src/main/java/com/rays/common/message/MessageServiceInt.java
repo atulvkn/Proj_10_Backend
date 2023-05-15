ba749b413093cc5b649a21a427cd2e1562bf5979
atulindore33 <atulindore33@gmail.com>
2023-05-15 05:52:13 +0530

    final project10 commited

---
+++ src/main/java/com/rays/common/message/MessageServiceInt.java
@@ -0,0 +1,25 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseServiceInt;
+import com.rays.common.UserContext;
+
+/**
+ * College Service interface.
+ * 
+ * Author Atul
+ */
+
+public interface MessageServiceInt extends BaseServiceInt<MessageDTO> {
+
+	/**
+	 * Finds Role by name.
+	 * 
+	 * @param name
+	 * @return
+	 * 
+	 */
+	public MessageDTO findByTitle(String name, UserContext userContext);
+
+	public MessageDTO findByCode(String code, UserContext userContext);
+
+}
